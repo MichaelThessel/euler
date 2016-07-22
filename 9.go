32b569e82be82ea3d114d0f0887289afa86f8cbf
Michael Thessel <michael.thessel@gmx.de>
2016-07-21 18:37:14 -1000

    9. solution

---
+++ 9.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+import "math"
+
+type triplet struct {
+    a int
+    b int
+    c int
+}
+
+func main() {
+    fmt.Println(pythagoreanTriplet())
+}
+
+func pythagoreanTriplet() int {
+    var t triplet
+
+    for t.c = 1000; t.c >= 0; t.c-- {
+        for t.b = t.c - 1; t.b >= 0; t.b-- {
+            for t.a = t.b - 1; t.a >= 0; t.a-- {
+                if
+                    math.Pow(float64(t.a), 2) + math.Pow(float64(t.b), 2) == math.Pow(float64(t.c), 2) &&
+                    t.a + t.b + t.c == 1000 {
+                        return t.a * t.b * t.c
+                 }
+
+            }
+        }
+    }
+
+    return -1
+}
