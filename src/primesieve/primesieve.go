8bd872ee3db1c0909db2a52b0b2db47ccfb18d3a
Michael Thessel <michael.thessel@gmx.de>
2016-08-07 14:43:30 -1000

    Improved primesieve

--- src/primesieve/primesieve.go
+++ src/primesieve/primesieve.go
@@ -1,30 +1,31 @@
 package primesieve
 
+import "math"
+
 func Primes(count int) []int {
-    return cull(filter(setup(count), 0))
+    return cull(filter(setup(count)))
 }
 
 func setup(count int) []int {
-    primes := make([]int, count - 1, count - 1)
+    primes := make([]int, count + 1, count + 1)
 
     for i := 2; i <= count; i++ {
-        primes[i - 2] = i
+        primes[i] = i
     }
 
     return primes
 }
 
-func filter(primes []int, index int) []int {
-    for i := index + 1; i < len(primes); i++ {
-        if primes[i] != 0 && primes[i] % primes[index] == 0 {
-            primes[i] = 0
-        }
-    }
-
-    for i := index + 1; i < len(primes); i++ {
+func filter(primes []int) []int {
+    primeCount := len(primes) - 1
+    for i := 2; i < int(math.Sqrt(float64(primeCount))); i++ {
         if primes[i] != 0 {
-            filter(primes, i)
-            break
+            it := 0
+            i2 := int(math.Pow(float64(i), 2))
+            for j:= i2; j <= primeCount; j = i2 + it * i  {
+                primes[j] = 0;
+                it++
+            }
         }
     }
 
